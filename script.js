ae34ad02d56baa8d1ed8c95515aa9c90814d0a20
Grant Stewart <gnstewart14@gmail.com>
2023-04-21 16:51:21 -0500

    Added input choices

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const playerSelection = parseInt(prompt("Rock, paper, scissors, shoot!")).toLowerCase;
+const select = document.querySelector('select');
 
 let computerSelection = Math.random();
 if (computerSelection < 0.34) {
@@ -11,16 +11,19 @@ if (computerSelection < 0.34) {
 
 console.log(computerSelection);
 
-function playround(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "Draw!";
-    } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
-        return "You win!";
+document.addEventListener("change", playRound);
+
+function playRound(select, computerSelection) {
+    
+    const results = document.querySelector('p');
+    const playerSelection = select.value; 
+    if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
+        results.textContent = "You win!";
     } else if (playerSelection === "scissors" && computerSelection === "rock" || playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lose!"
+        results.textContent = "You lose!"
     } else {
-        return "Why did you bring " + playerSelection + " to a rock, paper, scissors contest?";
+        results.textContent = "Draw!"
     }
-    }
+}
     
 
